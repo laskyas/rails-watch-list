5e34233ab9d985fedaceb9cad10b7f5e8d6abecd
Alexandra Lasky <alexandra.lasky@gmail.com>
2021-04-15 16:13:12 +0200

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :movies
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
