147779b87f3467b7ad4488f4d7d992c7b447bb5f
Alexandra Lasky <alexandra.lasky@gmail.com>
2021-04-16 11:48:34 +0200

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies
   resources :lists do
