65ba4cd4d4cad9f76929d8201a3796b2d8f33ee9
Alexandra Lasky <alexandra.lasky@gmail.com>
2021-04-17 20:26:41 +0200

    saving photo uploader

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,14 @@ class ListsController < ApplicationController
     @bookmark = Bookmark.new
   end
 
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def list_params
